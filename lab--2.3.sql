0db36fe12b9e4ec0c177c3d64393332e9bb25ed3
RidhamRupavatiya <108151918+RidhamRupavatiya@users.noreply.github.com>
2022-07-19 00:58:54 +0530

    Update lab--2.3.sql

--- lab--2.3.sql
+++ lab--2.3.sql
@@ -1,12 +1,10 @@
-delete from Customers where city='bombay';						--1st query
-delete from deposite where amount<=1000;						--2nd query
-delete from borrow where bname='ajani';							--3rd query
-delete from borrow where loanNo between 201 and 210;			--4th query
-delete from deposite where adate>='1-12-96';					--5th query
-truncate table customers;										--6th query
+delete from Customers where city='bombay';						      --1st query
+delete from deposite where amount<=1000;					        	--2nd query
+delete from borrow where bname='ajani';							        --3rd query
+delete from borrow where loanNo between 201 and 210;			  --4th query
+delete from deposite where adate>='1-12-96';				      	--5th query
+truncate table customers;								                		--6th query
 delete from deposite where cname='anil' and actno!=101;			--7th query
-delete from branch where bname!='andheri';						--8th query
+delete from branch where bname!='andheri';						      --8th query
 delete from borrow where amount<2000 and bname!='vrce';			--9th query
-drop table branch;											    --10th query
-
-select *from branch
+drop table branch;											                    --10th query
